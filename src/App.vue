7ec5ecc544228eb241b68ad382cde0a3eca7e00d
Jean-Philippe Dos Santos <jeanphilippeds@theodo.fr>
2018-10-09 17:10:44 +0200

    Create Vue App

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="game-of-life">
+    <div class="logos-container">
+      <img class="theodo-logo" alt="Theodo logo" src="./assets/theodo-logo.png">
+      <img class="vue-logo" alt="Vue logo" src="./assets/vue-logo.png">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'GameOfLife'
+}
+</script>
+
+<style>
+#game-of-life {
+  padding: 40px;
+  position: relative;
+}
+.logos-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.logos-container img {
+  height: 40px;
+}
+.logos-container img + img {
+  margin-left: 5px;
+}
+</style>
