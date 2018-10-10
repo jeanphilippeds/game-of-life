89519491583a3c91cce60d8325e12116ed548d82
JP <jeanphilippeds@theodo.fr>
2018-10-11 01:16:22 +0200

    Create service to implement the laws of Game of Life

---
+++ src/services/physic-laws.js
@@ -0,0 +1,10 @@
+const PhysicLaws = {}
+
+PhysicLaws.tick = function (previousGridState) {
+  const rowsCount = previousGridState.length
+  const columnsCount = previousGridState[0].length
+
+  return previousGridState
+}
+
+export default PhysicLaws
