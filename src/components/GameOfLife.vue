08dc0bbe561f81565cc7d626d306360fb0c29890
Romain Marquant <romainm@theodo.fr>
2018-10-15 20:46:00 +0200

    RANNGE

--- src/components/GameOfLife.vue
+++ src/components/GameOfLife.vue
@@ -5,28 +5,36 @@
       :name="isPaused ? 'Play' : 'Pause'"
       :click-callback="playGame"
     />
+    <range
+      :change-callback="changeSpeed"
+    />
   </div>
 </template>
 
 <script>
   import Grid from './Grid.vue'
   import BaseButton from './BaseButton.vue'
+  import Range from './Range.vue'
 
   export default {
     name: 'gameOfLife',
     components: {
       Grid,
-      BaseButton
+      BaseButton,
+      Range
     },
     data: function() {
       return {
-        isPaused: true
+        isPaused: true,
+        speed: 0
       }
     },
     methods: {
       playGame: function () {
         this.isPaused = !this.isPaused
-        window.console.log('Hello')
+      },
+      changeSpeed: function (value) {
+        this.speed = value
       }
     }
   }
---
+++ src/components/Range.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="range" id="speed" v-model="value" @input="handleChange"
+    min="0" max="100" step="2" />
+</template>
+
+<script>
+export default {
+  name: 'Range',
+  props: {
+    changeCallback: Function
+  },
+  methods: {
+    handleChange: function () {
+      this.changeCallback(parseInt(this.value))
+    }
+  },
+  data: function () {
+    return {
+        value: 0
+    }
+  }
+}
+</script>
+
