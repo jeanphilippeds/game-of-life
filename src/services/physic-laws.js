0f945ad334fdf951b236bf1df1f7a0264557e676
JP <jeanphilippeds@theodo.fr>
2018-10-15 19:21:03 +0200

    Implement law

--- src/services/physic-laws.js
+++ src/services/physic-laws.js
@@ -1,10 +1,28 @@
 const PhysicLaws = {}
 
-PhysicLaws.tick = function(previousGridState) {
-  const rowsCount = previousGridState.length
-  const columnsCount = previousGridState[0].length
 
-  return previousGridState
+PhysicLaws.tick = (iMax, jMax, aliveCellsIndexed) => {
+  const newAliveCellsIndexed = {}
+
+  for (var i=0; i < iMax; i++) {
+    for (var j=0; j < jMax; j++) {
+      const key = PhysicLaws.getIndex(i, j)
+      const isAliveCell = aliveCellsIndexed[key]
+      const aliveNeighboursCount = PhysicLaws.getAliveNeighboursCount(i, j)
+
+      newAliveCellsIndexed[key] = PhysicLaws.getState(isAliveCell, aliveNeighboursCount)
+    }
+  }
+
+  return newAliveCellsIndexed
+}
+
+PhysicLaws.getState = (isAliveCell, aliveNeighboursCount) => {
+  if (isAliveCell && aliveNeighboursCount < 2) return false
+  if (isAliveCell && aliveNeighboursCount > 3) return false
+  if (!isAliveCell && aliveNeighboursCount == 3) return true
+
+  return isAliveCell
 }
 
 PhysicLaws.getIndex = (row, column) => {
