4e250bd382e821964d7bc8853caa7ab8696479e2
gabriel andrin <gabriela@theodo.fr>
2018-10-15 19:01:07 +0200

    Lint + new function to get alive neighbours count

--- src/services/physic-laws.js
+++ src/services/physic-laws.js
@@ -1,23 +1,30 @@
 const PhysicLaws = {}
 
-PhysicLaws.tick = function (previousGridState) {
+PhysicLaws.tick = function(previousGridState) {
   const rowsCount = previousGridState.length
   const columnsCount = previousGridState[0].length
 
   return previousGridState
 }
 
-PhysicLaws.getNeighboursIndex = (row, column) => {
+PhysicLaws.getNeighbours = (row, column) => {
   return [
-    `R${row-1}C${column-1}`,
-    `R${row-1}C${column}`,
-    `R${row-1}C${column+1}`,
-    `R${row}C${column-1}`,
-    `R${row}C${column+1}`,
-    `R${row+1}C${column-1}`,
-    `R${row+1}C${column}`,
-    `R${row+1}C${column+1}`
+    `R${row - 1}C${column - 1}`,
+    `R${row - 1}C${column}`,
+    `R${row - 1}C${column + 1}`,
+    `R${row}C${column - 1}`,
+    `R${row}C${column + 1}`,
+    `R${row + 1}C${column - 1}`,
+    `R${row + 1}C${column}`,
+    `R${row + 1}C${column + 1}`
   ]
-};
+}
+
+PhysicLaws.getAliveNeighboursCount = (row, column, aliveCellsIndex) => {
+  const neighbours = PhysicLaws.getNeighbours(row, column)
+
+  return neighbours.filter(neighbourIndex => aliveCellsIndex[neighbourIndex])
+    .length
+}
 
 export default PhysicLaws
