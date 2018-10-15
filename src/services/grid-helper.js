83d92c1530019daef8177451cb47b85eae207105
gabriel andrin <gabriela@theodo.fr>
2018-10-15 20:42:19 +0200

    Add function to generate random initial state

--- src/services/grid-helper.js
+++ src/services/grid-helper.js
@@ -1,22 +1,37 @@
-const getNeighbours = (row, column) => {
-    return [
-        getIndex(row - 1, column - 1),
-        getIndex(row - 1, column),
-        getIndex(row - 1, column + 1),
-        getIndex(row, column - 1),
-        getIndex(row, column + 1),
-        getIndex(row + 1, column - 1),
-        getIndex(row + 1, column),
-        getIndex(row + 1, column + 1)
-    ]
-}
 export const getIndex = (row, column) => {
-    return `R${row}C${column}`
+  return `R${row}C${column}`
 }
 
-export const getAliveNeighboursCount = (row, column, aliveCellsIndex) => {
-    const neighbours = getNeighbours(row, column)
+const getNeighbours = (row, column) => {
+  return [
+    getIndex(row - 1, column - 1),
+    getIndex(row - 1, column),
+    getIndex(row - 1, column + 1),
+    getIndex(row, column - 1),
+    getIndex(row, column + 1),
+    getIndex(row + 1, column - 1),
+    getIndex(row + 1, column),
+    getIndex(row + 1, column + 1)
+  ]
+}
 
-    return neighbours.filter(neighbourIndex => aliveCellsIndex[neighbourIndex])
-        .length
+export const getAliveNeighboursCount = (row, column, aliveCellsIndexed) => {
+  const neighbours = getNeighbours(row, column)
+
+  return neighbours.filter(neighbourIndex => aliveCellsIndexed[neighbourIndex])
+    .length
+}
+
+export const getRandomIndex = (rowsCount, columnsCount) => {
+  const aliveCellsIndexed = {}
+
+  for (let row = 0; row < rowsCount; row++) {
+    for (let column = 0; column < columnsCount; column++) {
+      const isAlive = Math.random() >= 0.5
+      if (isAlive) {
+        aliveCellsIndexed[getIndex(row, column)] = true
+      }
+    }
+  }
+  return aliveCellsIndexed
 }
